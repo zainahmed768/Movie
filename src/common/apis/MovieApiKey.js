16764b96488db63fe4818c01b7ef1a8a9f70741f
zainahmed768 <zainahmed231@gmail.com>
2022-03-30 15:07:59 -0700

    this

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const apikey = 'c92fb3bd';
---
+++ src/components/Moviedetail/Moviedetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Moviedetail = () => {
+  return (
+    <div>Moviedetail</div>
+  )
+}
+
+export default Moviedetail
