1d71997b0fbcd855217814fdcd512d4a48f02c5c
zainahmed768 <zainahmed231@gmail.com>
2022-03-30 15:22:29 -0700

    all files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home/Home';
+import Header from './components/Header/header';
+import MovieDetail from './components/Moviedetail/Moviedetail'; 
+import pageNotFound from './components/pageNotFound/pageNotFound';
+// import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header></Header>
+        <Route path="/" component={Home} />
+        <Route path="/movie/:imdbID" component={MovieDetail} />
+        <Route component={pageNotFound} />
+      </Router>
     </div>
   );
 }
